5c607c504eb27e29d74149311271aaee0195637a
mtrop <mtropiano@gmail.com>
2014-09-23 02:41:58 -0400

    Added annotation scanning to TypeProfile.

--- src/com/blackrook/commons/TypeProfile.java
+++ src/com/blackrook/commons/TypeProfile.java
@@ -7,11 +7,14 @@
  ******************************************************************************/
 package com.blackrook.commons;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import com.blackrook.commons.Reflect;
 import com.blackrook.commons.hash.HashMap;
+import com.blackrook.commons.hash.HashedQueueMap;
+import com.blackrook.commons.linkedlist.Queue;
 
 /**
  * Type profile for an unknown object that has an ambiguous signature for 
@@ -26,12 +29,24 @@ public class TypeProfile<T extends Object>
 	private static final HashMap<Class<?>, TypeProfile<?>> 
 		REGISTERED_TYPES = new HashMap<Class<?>, TypeProfile<?>>();
 
+	/** Array of zero fields. */
+	private static final Field[] NO_FIELDS = new Field[0];
+	/** Array of zero method signatures. */
+	private static final MethodSignature[] NO_METHODS = new MethodSignature[0];
+	
 	/** Map of Public fields. */
 	private HashMap<String, Field> publicFields;
 	/** Map of getters. */
 	private HashMap<String, MethodSignature> getterMethods;
 	/** Map of setters. */
 	private HashMap<String, MethodSignature> setterMethods;
+	
+	/** Map of annotations to fields. */
+	private HashedQueueMap<Class<? extends Annotation>, Field> publicFieldAnnotations;
+	/** Map of annotations on getters. */
+	private HashedQueueMap<Class<? extends Annotation>, MethodSignature> getterAnnotations;
+	/** Map of annotations on setters. */
+	private HashedQueueMap<Class<? extends Annotation>, MethodSignature> setterAnnotations;
 	
 	/**
 	 * Gets a type profile for a type.
@@ -70,19 +85,37 @@ public class TypeProfile<T extends Object>
 	/** Creates a profile from a class. */
 	private TypeProfile(Class<? extends T> inputClass)
 	{
-		publicFields = new HashMap<String, Field>();
-		getterMethods = new HashMap<String, MethodSignature>();
-		setterMethods = new HashMap<String, MethodSignature>();
+		publicFields = new HashMap<String, Field>(4);
+		getterMethods = new HashMap<String, MethodSignature>(4);
+		setterMethods = new HashMap<String, MethodSignature>(4);
+
+		publicFieldAnnotations = new HashedQueueMap<Class<? extends Annotation>, Field>(4);
+		getterAnnotations = new HashedQueueMap<Class<? extends Annotation>, MethodSignature>(4);
+		setterAnnotations = new HashedQueueMap<Class<? extends Annotation>, MethodSignature>(4);
 		
 		for (Field f : inputClass.getFields())
+		{
 			publicFields.put(f.getName(), f);
+			for (Annotation a : f.getAnnotations())
+				publicFieldAnnotations.enqueue(a.annotationType(), f);
+		}
 		
 		for (Method m : inputClass.getMethods())
 		{
 			if (Reflect.isGetter(m) && !m.getName().equals("getClass"))
-				getterMethods.put(Reflect.getFieldName(m.getName()), new MethodSignature(m.getReturnType(), m));
+			{
+				MethodSignature ms = new MethodSignature(m.getReturnType(), m);
+				getterMethods.put(Reflect.getFieldName(m.getName()), ms);
+				for (Annotation a : m.getAnnotations())
+					getterAnnotations.enqueue(a.annotationType(), ms);
+			}
 			else if (Reflect.isSetter(m))
-				setterMethods.put(Reflect.getFieldName(m.getName()), new MethodSignature(m.getParameterTypes()[0], m));
+			{
+				MethodSignature ms = new MethodSignature(m.getParameterTypes()[0], m);
+				setterMethods.put(Reflect.getFieldName(m.getName()), ms);
+				for (Annotation a : m.getAnnotations())
+					setterAnnotations.enqueue(a.annotationType(), ms);
+			}
 		}
 	}
 	
@@ -114,6 +147,54 @@ public class TypeProfile<T extends Object>
 	public HashMap<String, MethodSignature> getSetterMethods()
 	{
 		return setterMethods;
+	}
+
+	/**
+	 * Returns an array of public fields on this type that contain a certain annotation class type.
+	 * @param annotation the annotation type to search for.
+	 * @since 2.20.0 
+	 */
+	public Field[] getAnnotatedPublicFields(Class<? extends Annotation> annotation)
+	{
+		Queue<Field> qf = publicFieldAnnotations.get(annotation);
+		if (qf == null || qf.isEmpty())
+			return NO_FIELDS;
+		
+		Field[] out = new Field[qf.size()];
+		qf.toArray(out);
+		return out;
+	}
+
+	/**
+	 * Returns an array of getter method signatures on this type that contain a certain annotation class type.
+	 * @param annotation the annotation type to search for.
+	 * @since 2.20.0 
+	 */
+	public MethodSignature[] getAnnotatedGetters(Class<? extends Annotation> annotation)
+	{
+		Queue<MethodSignature> qf = getterAnnotations.get(annotation);
+		if (qf == null || qf.isEmpty())
+			return NO_METHODS;
+		
+		MethodSignature[] out = new MethodSignature[qf.size()];
+		qf.toArray(out);
+		return out;
+	}
+
+	/**
+	 * Returns an array of setter method signatures on this type that contain a certain annotation class type.
+	 * @param annotation the annotation type to search for.
+	 * @since 2.20.0 
+	 */
+	public MethodSignature[] getAnnotatedSetters(Class<? extends Annotation> annotation)
+	{
+		Queue<MethodSignature> qf = setterAnnotations.get(annotation);
+		if (qf == null || qf.isEmpty())
+			return NO_METHODS;
+		
+		MethodSignature[] out = new MethodSignature[qf.size()];
+		qf.toArray(out);
+		return out;
 	}
 
 	/**
