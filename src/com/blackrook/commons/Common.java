eb185478eb123524350e49de44f42a0616e2a1de
mtrop <mtropiano@gmail.com>
2017-03-22 21:55:09 -0400

    Changed Common HTTP request functionality, added some methods.

--- src/com/blackrook/commons/Common.java
+++ src/com/blackrook/commons/Common.java
@@ -13,6 +13,7 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -791,60 +792,174 @@ public final class Common
 		Common.relay(in, bos);
 		return bos.toByteArray();
 	}
+
 	
-	/**
-	 * Gets the content from a opening an HTTP URL, using the default timeout
-	 * of 30 seconds (30000 milliseconds).
-	 * @param url the URL to open and read.
-	 * @return the content from opening an HTTP request.
-	 * @throws IOException if an error happens during the read, or a bad response is returned (not 2xx).
-	 * @throws SocketTimeoutException if the socket read times out.
+    // Keep alphabetical.
+    protected static final String[] VALID_HTTP = new String[]{"http", "https"};
+
+    /**
+     * Gets the content from a opening an HTTP URL, using the default timeout of 30 seconds (30000 milliseconds).
+     * @param url the URL to open and read.
+     * @return the content from opening an HTTP request.
+     * @throws IOException if an error happens during the read, or a bad response is returned (not 2xx).
+     * @throws SocketTimeoutException if the socket read times out.
 	 * @since 2.5.0
-	 */
-	public static String getHTTPContent(URL url) throws IOException, SocketTimeoutException
-	{
-		return getHTTPContent(url, 30000);
-	}
-	
-	/**
-	 * Gets the content from a opening an HTTP URL.
-	 * @param url the URL to open and read.
-	 * @param socketTimeoutMillis the socket timeout time in milliseconds. 0 is forever.
-	 * @return the content from opening an HTTP request.
-	 * @throws IOException if an error happens during the read, or a bad response is returned (not 2xx).
-	 * @throws SocketTimeoutException if the socket read times out.
+     */
+    public static String getHTTPContent(URL url) throws IOException
+    {
+        return getHTTPContent(url, "GET", null, null, null, null, "UTF-8", 30000).getStringContent();
+    }
+
+    /**
+     * Gets the content from a opening an HTTP URL.
+     * @param url the URL to open and read.
+     * @param socketTimeoutMillis the socket timeout time in milliseconds. 0 is forever.
+     * @return the content from opening an HTTP request.
+     * @throws IOException if an error happens during the read, or a bad response is returned (not 2xx/3xx).
+     * @throws SocketTimeoutException if the socket read times out.
+     * @throws UnsupportedEncodingException if the content cannot be decoded into a string using the provided encoding.
 	 * @since 2.6.0
-	 */
-	public static String getHTTPContent(URL url, int socketTimeoutMillis) throws IOException, SocketTimeoutException
-	{
-		if (!url.getProtocol().equalsIgnoreCase("http") && !url.getProtocol().equalsIgnoreCase("https"))
-			throw new IOException("This is not an HTTP URL.");
-		
-		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-		conn.setReadTimeout(socketTimeoutMillis);
-		
-		int code = conn.getResponseCode();
-		String codeString = conn.getResponseMessage();
-		String encoding = conn.getContentEncoding();
-		
-		if (!(code >= 200 && code < 300))
-		{
-			conn.disconnect();
-			throw new IOException("Response was code "+code+": "+codeString);
-		}
-		
-		InputStream in = null;
-		try {
-			in = conn.getInputStream();
-			return getTextualContents(in, encoding);
-		} catch (IOException e) {
-			throw e;
-		} finally {
-			Common.close(in);
-			conn.disconnect();
-		}
-		
-	}
+     */
+    public static String getHTTPContent(URL url, int socketTimeoutMillis) throws IOException
+    {
+        return getHTTPContent(url, "GET", null, null, null, null, "UTF-8", socketTimeoutMillis).getStringContent();
+    }
+
+    /**
+     * Gets the content from a opening an HTTP URL.
+     * @param url the URL to open and read.
+     * @param socketTimeoutMillis the socket timeout time in milliseconds. 0 is forever.
+     * @param defaultResponseCharset if the response charset is not specified, use this one.
+     * @return the content from opening an HTTP request.
+     * @throws IOException if an error happens during the read, or a bad response is returned (not 2xx/3xx).
+     * @throws SocketTimeoutException if the socket read times out.
+     * @throws UnsupportedEncodingException if the content cannot be decoded into a string using the provided encoding.
+	 * @since 2.6.0
+     */
+    public static String getHTTPContent(URL url, String defaultResponseCharset, int socketTimeoutMillis) throws IOException
+    {
+        return getHTTPContent(url, "GET", null, null, null, defaultResponseCharset, "UTF-8", socketTimeoutMillis).getStringContent();
+    }
+
+    /**
+     * Gets the content from a opening an HTTP URL, automatically handling redirects (301, 307, 308).
+     * @param url the URL to open and read.
+     * @param requestMethod the request method.
+     * @param data if not null, send this object as JSON in the content.
+     * @param dataContentType if data is not null, this is the content type. If this is null, uses "application/octet-stream".
+     * @param dataContentEncoding if data is not null, this is the encoding for the written data. Can be null.
+     * @param defaultResponseEncoding if the response encoding is not specified, use this one.
+     * @param defaultResponseCharset if the response charset is not specified, use this one.
+     * @param socketTimeoutMillis the socket timeout time in milliseconds. 0 is forever.
+     * @return the content from opening an HTTP request.
+     * @throws IOException if an error happens during the read.
+     * @throws SocketTimeoutException if the socket read times out.
+     * @throws UnsupportedEncodingException if the content cannot be decoded into a string using the provided encoding.
+     * @throws StackOverflowError if a redirect loop occurs.
+     * @since 2.31.1
+     */
+    public static HTTPResponse getResolvedHTTPContent(URL url, String requestMethod, byte[] data, String dataContentType, String dataContentEncoding, String defaultResponseEncoding, String defaultResponseCharset, int socketTimeoutMillis) throws IOException
+    {
+        HTTPResponse response = getHTTPContent(url, requestMethod, data, dataContentType, dataContentEncoding, defaultResponseEncoding, defaultResponseCharset, socketTimeoutMillis);
+
+        // redirect permanent or redirect temporary
+        if (response.statusCode == 301 || response.statusCode == 307 || response.statusCode == 308)
+        {
+            if (response.location == null)
+                throw new IOException("Response was code "+response.statusCode+", but no location specified!");
+
+            URL nextLocation = null;
+            try {
+                nextLocation = new URL(response.location);
+            } catch (MalformedURLException ex) {
+                throw new IOException("Redirect location was malformed!", ex);
+            }
+
+            HTTPResponse redirectedResponse = getResolvedHTTPContent(nextLocation, requestMethod, data, dataContentType, dataContentEncoding, defaultResponseEncoding, defaultResponseCharset, socketTimeoutMillis);
+            redirectedResponse.referrer = url;
+            redirectedResponse.redirected = true;
+            return redirectedResponse;
+        }
+
+        return response;
+    }
+
+    /**
+     * Gets the content from a opening an HTTP URL.
+     * @param url the URL to open and read.
+     * @param requestMethod the request method.
+     * @param data if not null, send this data in the content.
+     * @param dataContentType if data is not null, this is the content type. If this is null, uses "application/octet-stream".
+     * @param dataContentEncoding if data is not null, this is the encoding for the written data. Can be null.
+     * @param defaultResponseEncoding if the response encoding is not specified, use this one.
+     * @param defaultResponseCharset if the response charset is not specified, use this one.
+     * @param socketTimeoutMillis the socket timeout time in milliseconds. 0 is forever.
+     * @return the content from opening an HTTP request.
+     * @throws IOException if an error happens during the read/write.
+     * @throws SocketTimeoutException if the socket read times out.
+     * @throws ProtocolException if the requestMethod is incorrect.
+     */
+    public static HTTPResponse getHTTPContent(URL url, String requestMethod, byte[] data, String dataContentType, String dataContentEncoding, String defaultResponseEncoding, String defaultResponseCharset, int socketTimeoutMillis) throws IOException
+    {
+        if (Arrays.binarySearch(VALID_HTTP, url.getProtocol()) < 0)
+            throw new IOException("This is not an HTTP URL.");
+
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setReadTimeout(socketTimeoutMillis);
+        conn.setRequestMethod(requestMethod);
+
+        // set up POST data.
+        if (data != null)
+        {
+            conn.setRequestProperty("Content-Length", String.valueOf(data.length));
+            conn.setRequestProperty("Content-Type", dataContentType == null ? "application/octet-stream" : dataContentType);
+            if (dataContentEncoding != null)
+                conn.setRequestProperty("Content-Encoding", dataContentEncoding);
+            conn.setDoOutput(true);
+            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+            dos.write(data);
+            dos.close();
+        }
+
+        HTTPResponse out = new HTTPResponse();
+        out.statusCode = conn.getResponseCode();
+        out.statusMessage = conn.getResponseMessage();
+        out.encoding = conn.getContentEncoding();
+        out.contentType = conn.getContentType();
+        
+        int charsetindex;
+        if ((charsetindex = out.contentType.toLowerCase().indexOf("charset=")) >= 0)
+        {
+        	int endIndex = out.contentType.indexOf(";", charsetindex);
+        	if (endIndex >= 0)
+        		out.charset = out.contentType.substring(charsetindex + "charset=".length(), endIndex).trim();
+        	else
+        		out.charset = out.contentType.substring(charsetindex + "charset=".length()).trim();
+        }
+        
+        if (out.charset == null)
+            out.charset = defaultResponseCharset;
+        
+        out.location = conn.getHeaderField("Location"); // if any.
+
+        int range = out.statusCode / 100;
+        if (range >= 2 && range <= 3)
+        {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            BufferedInputStream in = null;
+            try {
+                in = new BufferedInputStream(conn.getInputStream());
+                relay(in, bos);
+                out.content = bos.toByteArray();
+            } finally {
+                close(in);
+            }
+        }
+
+        conn.disconnect();
+
+        return out;
+    }
 
 	/**
 	 * Gets the byte content from a opening an HTTP URL, using the default timeout
@@ -3418,7 +3533,19 @@ public final class Common
 	public static void close(Closeable c)
 	{
 		if (c == null) return;
-		try { c.close(); 	} catch (IOException e){}
+		try { c.close(); } catch (IOException e){}
+	}
+	
+	/**
+	 * Attempts to close an {@link AutoCloseable} object.
+	 * If the object is null, this does nothing.
+	 * @param c the reference to the AutoCloseable object.
+	 * @since 2.31.1
+	 */
+	public static void close(AutoCloseable c)
+	{
+		if (c == null) return;
+		try { c.close(); } catch (Exception e){}
 	}
 	
 	/**
@@ -3430,7 +3557,7 @@ public final class Common
 	public static void close(Connection c)
 	{
 		if (c == null) return;
-		try { c.close(); 	} catch (SQLException e){}
+		try { c.close(); } catch (SQLException e){}
 	}
 	
 	/**
@@ -3442,7 +3569,7 @@ public final class Common
 	public static void close(Statement s)
 	{
 		if (s == null) return;
-		try { s.close(); 	} catch (SQLException e){}
+		try { s.close(); } catch (SQLException e){}
 	}
 	
 	/**
@@ -3454,7 +3581,7 @@ public final class Common
 	public static void close(ResultSet rs)
 	{
 		if (rs == null) return;
-		try { rs.close(); 	} catch (SQLException e){}
+		try { rs.close(); } catch (SQLException e){}
 	}
 	
 	/**
@@ -3525,6 +3652,108 @@ public final class Common
 	public static void noop()
 	{
 		// Do nothing.
+	}
+
+	/**
+	 * Response from an HTTP call.
+	 * @since 2.31.1
+	 */
+	public static class HTTPResponse
+	{
+	    private int statusCode;
+	    private String statusMessage;
+	    private String location;
+	    private URL referrer;
+	    private String charset;
+	    private String contentType;
+	    private String encoding;
+	    private byte[] content;
+	    private boolean redirected;
+
+	    /**
+	     * Gets the string contents of the response, decoded using the response's charset.
+	     * @return the content of the response as a native string, decoded.
+	     * @throws UnsupportedEncodingException if the response is not in an understood charset.
+	     */
+	    public String getStringContent() throws UnsupportedEncodingException
+	    {
+	    	if (charset == null)
+	    		throw new UnsupportedEncodingException("No charset specified.");
+	        return content != null ? new String(content, charset) : null;
+	    }
+	
+	    /**
+	     * @return the response status code.
+	     */
+	    public int getStatusCode()
+	    {
+	        return statusCode;
+	    }
+	
+	    /**
+	     * @return the response status message.
+	     */
+	    public String getStatusMessage()
+	    {
+	        return statusMessage;
+	    }
+	
+	    /**
+	     * @return the url of the next location, if this is a 3xx redirect response.
+	     */
+	    public String getLocation()
+	    {
+	        return location;
+	    }
+	
+	    /**
+	     * @return the response's charset. can be null.
+	     */
+	    public String getCharset()
+	    {
+	        return charset;
+	    }
+	
+	    /**
+	     * @return the response's content type. can be null.
+	     */
+	    public String getContentType()
+	    {
+	        return contentType;
+	    }
+	
+	    /**
+	     * @return the response's encoding. can be null.
+	     */
+	    public String getEncoding()
+	    {
+	        return encoding;
+	    }
+	
+	    /**
+	     * @return the response's content. can be null.
+	     */
+	    public byte[] getContent()
+	    {
+	        return content;
+	    }
+	
+	    /**
+	     * @return the response's referrer URL. can be null.
+	     */
+	    public URL getReferrer()
+	    {
+	        return referrer;
+	    }
+	
+	    /**
+	     * @return true if this response was the result of a redirect, false otherwise.
+	     */
+	    public boolean isRedirected()
+	    {
+	        return redirected;
+	    }
+	
 	}
 	
 }
